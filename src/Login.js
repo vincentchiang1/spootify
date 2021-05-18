b983612bcb49eabe6c626816eda914cd43397a7a
Vincent <vincentchiang1@gmail.com>
2021-05-17 23:29:23 -0700

    set up spotify server

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,24 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
 import { Link } from 'react-router-dom'
+const axios = require('axios');
 
 export default function Login() {
+
+    const callLogin = () => {
+        console.log('hihihhi')
+        axios.get('http://localhost:8888/login').then(resp => {
+
+            console.log(resp.data);
+        });
+    }
+
     return (
         <div>
             <h1>Spootify</h1>
             <Link to="/home">
-                <Button variant="contained" color="primary">Login</Button>
+                <Button variant="contained" color="primary" onClick={() => callLogin()}>CONNECT WITH SPOOTIFY</Button>
             </Link>         
         </div>
-    )
+    );
 }
