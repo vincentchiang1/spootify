69e1ea625c3f758ea5efa3244829c8df470357c5
John <likisongproduct@gmail.com>
2021-05-19 19:07:49 -0700

    working top tracks

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from '@material-ui/core';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import hash from './hash';
+import './Home.css';
 const axios = require('axios');
 
 export default function Home() {
   const [token, setToken] = useState('');
+  const [topTracks, setTopTracks] = useState([]);
+
+  const parsetopTracks = (tracks) => {
+    // console.log(tracks);
+    let tracksArray = [];
+    for (let item of tracks.items) {
+      let trackObj = {};
+      trackObj['songName'] = item.name;
+      trackObj['artistName'] = item.artists[0].name;
+      let imageObj = {};
+      imageObj['largeImg'] = item.album.images[0];
+      imageObj['mediumImg'] = item.album.images[1];
+      imageObj['smallImg'] = item.album.images[2];
+      trackObj['images'] = imageObj;
+      tracksArray.push(trackObj);
+    }
+    setTopTracks(tracksArray);
+    console.log(tracksArray);
+  };
 
   useEffect(() => {
     const getUserPlaylists = async () => {
@@ -14,16 +36,18 @@ export default function Home() {
         let config = {
           headers: { Authorization: 'Bearer ' + token },
           params: {
+            time_range: 'long_term',
             limit: 10,
-            offset: 0,
           },
         };
 
         let res = await axios.get(
-          'https://api.spotify.com/v1/me/playlists',
+          '	https://api.spotify.com/v1/me/top/tracks',
           config
         );
-        console.log(res);
+        if (res.data) {
+          parsetopTracks(res.data);
+        }
       }
     };
 
@@ -33,15 +57,29 @@ export default function Home() {
   }, [token]);
 
   return (
-    <div>
-      <h1>Home</h1>
-      <Button
-        onClick={() => {
-          console.log(token);
-        }}
-      >
-        HELLO
-      </Button>
+    <div className="home-page">
+      <h1>Top Tracks</h1>
+      <Tabs indicatorColor="primary">
+        <Tab label="All Time" />
+        <Tab label="Recent" />
+        <Tab label="Past 6 Months" />
+      </Tabs>
+      <div className="top-track-list">
+        {topTracks.map((track, index) => {
+          return (
+            <div key={index} className="track-container">
+              <div className="track-index-container">
+                <h1>{index + 1}</h1>
+              </div>
+              <img src={track.images.mediumImg.url} alt="track image" />
+              <div className="name-container">
+                <h1>{track.songName}</h1>
+                <h1>{track.artistName}</h1>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
