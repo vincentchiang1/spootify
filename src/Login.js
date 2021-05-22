d10480694f643616494254c4343348e177ee25bf
John <likisongproduct@gmail.com>
2021-05-21 22:48:03 -0700

    styled landing page

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,20 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
 import { authEndpoint, clientId, redirectUri, scopes } from './config';
-
+import './login.css';
 export default function Login() {
   return (
-    <div>
-      <h1>Spootify</h1>
-      <a
-        href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-          '%20'
-        )}&response_type=token&show_dialog=true`}
-      >
-        <Button variant="contained" color="primary">
-          CONNECT WITH SPOOTIFY
-        </Button>
-      </a>
+    <div className="login-container">
+      <div className="login-content">
+        <span className="login-title">Replay your Spootify Hits</span>
+        <a
+          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+            '%20'
+          )}&response_type=token&show_dialog=true`}
+        >
+          <button className="btn">Connect with Spootify</button>
+        </a>
+      </div>
     </div>
   );
 }
