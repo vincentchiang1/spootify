fa69790ef60b6aa775cc89a76c9052fac5b4be2f
John <likisongproduct@gmail.com>
2021-05-26 21:47:10 -0700

    routing works yay

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Login from './Login';
 import { Route, Switch } from 'react-router-dom';
+import { Redirect } from 'react-router';
+
 import Home from './Home';
+
+const LoginContainer = () => (
+  <div className="container">
+    <Route path="/" render={() => <Redirect to="/login" />} />
+    <Route path="/login" component={Login} />
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/" component={Login} exact />
-        <Route path="/home" component={Home} />
+        <Route exact path="/(login)" component={LoginContainer} />
+        <Route path="/home" component={Home}></Route>
       </Switch>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,20 @@ import './Home.css';
 import Sidebar from './Sidebar';
 import TopTracks from './TopTracks';
 import Routes from './Routes';
+import Artists from './Artists';
+import Recent from './Recent';
+import { Route, useRouteMatch, Switch } from 'react-router-dom';
 
 export default function Home() {
+  let match = useRouteMatch();
   return (
     <div className="home-page">
       <Sidebar />
-      <TopTracks />
-      {/* <Routes /> */}
+      <Switch>
+        <Route exact path={match.url + '/top-tracks'} component={TopTracks} />
+        <Route exact path={match.url + '/artists'} component={Artists} />
+        <Route exact path={match.url + '/recent'} component={Recent} />
+      </Switch>
     </div>
   );
 }
