d4357bd0b1a881a7b125b4d74007690987bd2938
jarhun88 <likisongproduct@gmail.com>
2021-05-28 19:32:56 -0700

    redirect

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const authEndpoint = 'https://accounts.spotify.com/authorize';
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = '884c025a2da34bc8bce1bfdf7ed93842';
 // export const redirectUri = 'http://localhost:3000/home/top-tracks';
-export const redirectUri = 'https://spootify.netlify.app/home/top-tracks';
+export const redirectUri = 'https://spoootify.netlify.app/home/top-tracks';
 export const scopes = [
   'user-top-read',
   'user-read-currently-playing',
